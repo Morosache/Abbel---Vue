4748efcf3313c41d3def814b588df01748a07cee
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-10-24 20:52:54 +0300

    Fixed bug 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <script setup>
 import {ref, onMounted, onUnmounted} from 'vue';
-import carousel1 from '/public/images/carousell1.jpg'
-import carousel2 from '/public/images/carousell2.jpg'
-import carousel3 from '/public/images/carousell3.jpg'
+import carousel1 from 'public/images/carousell1.jpg'
+import carousel2 from 'public/images/carousell2.jpg'
+import carousel3 from 'public/images/carousell3.jpg'
 
 const photo1 = carousel1;
 const photo2 = carousel2;
@@ -24,7 +24,7 @@ onUnmounted(() => clearInterval(interval));
     <div class="h-[70vh]">
         <!--Menu bar-->
         <div class="fixed z-50 w-screen h-[65px] bg-[#ffffff]/[10%] flex flex-row justify-between items-center">
-            <img src="/public/images/logo.png" alt="logo" class="w-[70px] h-[60px] opacity-[100%] mx-[15px]"></img>
+            <img src="public/images/logo.png" alt="logo" class="w-[70px] h-[60px] opacity-[100%] mx-[15px]"></img>
             <i class="bi bi-list text-white text-[30px] mx-[15px]"></i>
         </div>
          <transition name="fade" mode="out-in">
